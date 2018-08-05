ff1fa82423b562e1ec374d87b9aea7df12297ec0
mglavall <mgomezlavall@gmail.com>
2018-08-05 17:13:25 +0200

    Save session data in Identity service cache

--- src/app/core/services/identity.service.ts
+++ src/app/core/services/identity.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { User } from "../model/user.model";
+import { tap } from "rxjs/operators";
+import { Session } from "../model/session.model";
 
 @Injectable({
   providedIn: "root"
 })
 export class IdentityService {
+  session: Session;
   constructor(private httpClient: HttpClient) {}
 
   login(username: string, password: string) {
-    let log = {
-      username: username,
-      password: password,
-      client: "webapp"
-    };
-    debugger;
-    this.httpClient
-      .post("https://internal-api-staging-lb.interact.io/v2/login", log)
-      .subscribe(a => {
-        debugger;
-      });
+    return this.httpClient
+      .post<Session>("https://internal-api-staging-lb.interact.io/v2/login", {
+        username: username,
+        password: password,
+        client: "webapp"
+      })
+      .pipe(tap((val: Session) => (this.session = val)));
   }
 }
