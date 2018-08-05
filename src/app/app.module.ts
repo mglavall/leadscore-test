0414dbef9bdc72ab30b98852df600471317c0fe1
mglavall <mgomezlavall@gmail.com>
2018-08-06 01:15:36 +0200

    Add interceptor to add the authToken to every call
    The interceptor adds the authToken to the header of all the calls except the excluded ones. (The ones on the variable excluded)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { TokenInterceptor } from "./core/interceptors/token.interceptor";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from "@angular/common/http";
+import { IdentityService } from "../services/identity.service";
+import { Observable } from "rxjs/internal/Observable";
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(public identity: IdentityService) {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const excluded = "/login";
+    debugger;
+    // Exclude interceptor for login request:
+    if (request.url.search(excluded) === -1) {
+      request = request.clone({
+        setHeaders: {
+          authToken: `${this.identity.getAuthToken()}`
+        }
+      });
+    }
+
+    return next.handle(request);
+  }
+}
